7cc1744b286df142a74f7c2918a4cdbb3fed27a5
JgPhil <jamingph@gmail.com>
2021-04-04 17:35:41 +0200

    page: order_show

--- src/Controller/AccountOrderController.php
+++ src/Controller/AccountOrderController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
 use App\Repository\OrderRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,14 +13,36 @@ use Symfony\Component\Validator\Constraints\NotNull;
 class AccountOrderController extends AbstractController
 {
 
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/compte/mes-commandes", name="account_order")
      */
-    public function index(OrderRepository $orderRepo): Response
+    public function index(): Response
     {
-        $orders = $orderRepo->findSuccessOrders($this->getUser());
+        $orders = $this->entityManager->getRepository(Order::class)->findSuccessOrders($this->getUser());
         return $this->render('account/order.html.twig', [
             'orders' => $orders
+        ]);
+    }
+
+    /**
+     * @Route("/compte/mes-commandes/{reference}", name="account_order_show")
+     */
+    public function show($reference): Response
+    {
+        $order = $this->entityManager->getRepository(Order::class)->findOneByReference($reference);
+        if (!$order || $order->getUser() !== $this->getUser()) {
+            return $this->redirectToRoute('account_order');
+        }
+
+        return $this->render('account/show.html.twig', [
+            'order' => $order
         ]);
     }
 }
